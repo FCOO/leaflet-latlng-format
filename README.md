f80e692fc386fe34bc23ad35999fc70de0a61697
Niels Holt <niels@steenbuchholt.dk>
2016-10-06 09:45:23 +0200

    New methods: format(), latFormat(), lngFormat() Deprecated: asFormat(), latAsFormat(), lngAsFormat()

--- README.md
+++ README.md
@@ -13,42 +13,42 @@ Extension to L.LatLng to format lat,lng as text using [fcoo/latlng-format](https
 http://FCOO.github.io/leaflet-latlng-format/demo/ 
 
 ## Usage
-	var myLatLng = L.LatLng(54.25239, 9.646);
-	console.log( myLatLng.latAsFormat() ); //54°15'08,6"N
-	console.log( myLatLng.lngAsFormat() ); //9°38'45,5"E
+    var myLatLng = L.LatLng(54.25239, 9.646);
+    console.log( myLatLng.latFormat() ); //54°15'08,6"N
+    console.log( myLatLng.lngFormat() ); //9°38'45,5"E
 
-	L.LatLng.setFormat( L.LatLng.FORMAT_SMM	); //Change format to Degrees Decimal minutes (N65 30.258')	
+    L.LatLng.setFormat( L.LatLng.FORMAT_SMM    ); //Change format to Degrees Decimal minutes (N65 30.258')    
 
 
 
 ### L.LatLng
 
 `L.LatLng`is extended with const for the different formats and a method to change format
-	
-	L.LatLng.FORMAT_DMSS	//Degrees Minutes Seconds Decimal Seconds
-	L.LatLng.FORMAT_SMM		//Degrees Decimal minutes
-	L.LatLng.FORMAT_DD		//Decimal degrees
+    
+    L.LatLng.FORMAT_DMSS //Degrees Minutes Seconds Decimal Seconds
+    L.LatLng.FORMAT_SMM  //Degrees Decimal minutes
+    L.LatLng.FORMAT_DD   //Decimal degrees
 
-	L.LatLng.setFormat( formatId [, map] ); //Sets the format to formatId
-	L.LatLng.changeFormat([map]);           //Change the format to next format (FORMAT_DMSS -> FORMAT_SMM -> FORMAT_DD -> FORMAT_DMSS) 
+    L.LatLng.setFormat( formatId [, map] ); //Sets the format to formatId
+    L.LatLng.changeFormat([map]);           //Change the format to next format (FORMAT_DMSS -> FORMAT_SMM -> FORMAT_DD -> FORMAT_DMSS) 
 
 #### Event `latlngformatchange`
 if `map` is provided to `L.LatLng.setFormat` or `L.LatLng.changeFormat` a special event `latlngformatchange` is fired `map.fire('latlngformatchange', {format: L.LatLng.format});`
 
 E.g. 
-	map.on('latlngformatchange', function( data ){ ... }),
+    map.on('latlngformatchange', function( data ){ ... }),
 where `data.format` is the current [LatLngFormat-object](https://github.com/FCOO/latlng-format) 
 
 
 ### L.LatLng.prototype
 `L.LatLng.prototype` is extended with tree methods and an object
 
-	.latAsFormat()	//return this.lat formatted
-	.lngAsFormat()	//return this.lng formatted
-	.asFormat()		//return [this.latAsFormat(), this.lngAsFormat()]
+    .latFormat()    //return this.lat formatted
+    .lngFormat()    //return this.lng formatted
+    .format()       //return [this.latFormat(), this.lngFormat()]
 
 
-	.format = new LatLngFormat(..); 
+    .format = new LatLngFormat(..); 
 See [fcoo/latlng-format](https://github.com/FCOO/latlng-format) for the methods available in `.format`
 
 
@@ -60,16 +60,3 @@ Copyright (c) 2015 [FCOO](https://github.com/FCOO)
 ## Contact information
 
 Niels Holt nho@fcoo.dk
-
-
-## Credits and acknowledgements
-
-
-## Known bugs
-
-## Troubleshooting
-
-## Changelog
-
-
-
--- src/leaflet-latlng-format.js
+++ src/leaflet-latlng-format.js
@@ -1,43 +1,48 @@
 /****************************************************************************
-	leaflet-latlng-format.js,
+    leaflet-latlng-format.js,
 
-	(c) 2016, FCOO
+    (c) 2016, FCOO
 
-	https://github.com/FCOO/leaflet-latlng-format
-	https://github.com/FCOO
+    https://github.com/FCOO/leaflet-latlng-format
+    https://github.com/FCOO
 
 ****************************************************************************/
-;(function (L, window, document, undefined) {
-	"use strict";
+(function (L, window/*, document, undefined*/) {
+    "use strict";
 
 
-	//Extend L.LatLng
-	L.extend(L.LatLng, {
-		//Const to define differnet formats
-		FORMAT_DMSS	: window.LATLNGFORMAT_DMSS, //Degrees Minutes Seconds Decimal Seconds	:	N65d30'15.3"  d='degree sign'
-		FORMAT_SMM	: window.LATLNGFORMAT_DMM,	//Degrees Decimal minutes									:	N65d30.258'
-		FORMAT_DD		: window.LATLNGFORMAT_DD,		//Decimal degrees													: N41.1234d
-		format			: new window.LatLngFormat( window.LATLNGFORMAT_DMSS ),
-		setFormat		: function( formatId, map ){ 
-										L.LatLng.format.setFormat( formatId );
-										if (map)
-										  map.fireEvent('latlngformatchange', { format: this.format } );
-									},
-		changeFormat: function( map ){ 
-										this.setFormat( (this.format.options.formatId + 1) % (this.FORMAT_DD + 1), map ); 
-									}
-	});
+    //Extend L.LatLng
+    L.extend(L.LatLng, {
+        //Const to define differnet formats
+        FORMAT_DMSS: window.LATLNGFORMAT_DMSS, //Degrees Minutes Seconds Decimal Seconds: N65d30'15.3"  d='degree sign'
+        FORMAT_SMM : window.LATLNGFORMAT_DMM,  //Degrees Decimal minutes                : N65d30.258'
+        FORMAT_DD  : window.LATLNGFORMAT_DD,   //Decimal degrees                        : N41.1234d
+        format     : new window.LatLngFormat( window.LATLNGFORMAT_DMSS ),
+        setFormat  : function( formatId, map ){ 
+                         L.LatLng.format.setFormat( formatId );
+                         if (map)
+                             map.fireEvent('latlngformatchange', { format: this.format } );
+                      },
+        changeFormat: function( map ){ 
+                        this.setFormat( (this.format.options.formatId + 1) % (this.FORMAT_DD + 1), map ); 
+                      }
+    });
 
 
 
 
-	//Extend the prototype of L.LatLng
-	L.extend( L.LatLng.prototype, {
-		latAsFormat	: function(){ return L.LatLng.format.asTextLat( this.lat ); },
-		lngAsFormat	: function(){ return L.LatLng.format.asTextLng( this.lng ); },
-		asFormat		: function(){ return L.LatLng.format.asText( [this.lat, this.lng] ); }
+    //Extend the prototype of L.LatLng
+    L.extend( L.LatLng.prototype, {
+        latFormat: function(){ return L.LatLng.format.latFormat( this.lat );             },
+        lngFormat: function(){ return L.LatLng.format.lngFormat( this.lng );             },
+        format   : function(){ return L.LatLng.format.format   ( [this.lat, this.lng] ); },
 
-	});
+        //DEPRECATED: asFormat, latAsFormat, lngAsFormat
+        latAsFormat: function(){ return L.LatLng.format.asTextLat( this.lat ); },
+        lngAsFormat: function(){ return L.LatLng.format.asTextLng( this.lng ); },
+        asFormat   : function(){ return L.LatLng.format.asText( [this.lat, this.lng] ); }
+   
+    });
 
 }(L, this, document));
 
